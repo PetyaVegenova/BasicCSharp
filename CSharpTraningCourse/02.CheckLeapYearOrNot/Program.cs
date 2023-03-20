0d95b94f72ef322ebbf6911ceee8ca9a068a55dd
Petya Vegenova <Petya.Vegenova@iris.co.uk>
2023-03-20 17:54:49 +0200

    Fix logic

--- CSharpTraningCourse/02.CheckLeapYearOrNot/Program.cs
+++ CSharpTraningCourse/02.CheckLeapYearOrNot/Program.cs
@@ -6,7 +6,7 @@
         {
             int checkYear = int.Parse(Console.ReadLine());
 
-            if ((checkYear % 4 == 0) && (checkYear % 100 != 0) || (checkYear % 400 == 0))
+            if (((checkYear % 4 == 0) && (checkYear % 100 != 0)) || (checkYear % 400 == 0))
             {
                 Console.WriteLine($"Year {checkYear} is leap year!");
             }
---
+++ CSharpTraningCourse/03.WhatIsThe47-thPrimeNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03.WhatIsThe47_thPrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
