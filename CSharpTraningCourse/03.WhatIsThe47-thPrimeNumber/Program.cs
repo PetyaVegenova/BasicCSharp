e342fa9b61bc537292b5a22050d520ba86503c44
Petya Vegenova <Petya.Vegenova@iris.co.uk>
2023-03-20 18:34:07 +0200

    Add The 47-th prime number

--- CSharpTraningCourse/03.WhatIsThe47-thPrimeNumber/Program.cs
+++ CSharpTraningCourse/03.WhatIsThe47-thPrimeNumber/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool isPrime = false;
+            int primeIndex = 0;
+            int primeValue = 1;
+
+            while (primeIndex < 47)
+            {
+                primeValue++;
+
+                isPrime = IsPrime(primeValue);
+
+                if (isPrime)
+                {
+                    primeIndex++;
+                }
+            }
+
+            Console.WriteLine($"The 47-th prime number is {primeValue}");
+        }
+
+        private static bool IsPrime(int primeValue)
+        {
+            var result = true;
+
+            for (int i = 2; i <= Math.Sqrt(primeValue); i++)
+            {
+                if (primeValue % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
