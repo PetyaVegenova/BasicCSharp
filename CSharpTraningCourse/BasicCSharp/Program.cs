e15ddebb86595731e46842fe52fb8fdc9b5070a0
Petya Vegenova <Petya.Vegenova@iris.co.uk>
2023-04-21 18:18:48 +0300

    update

--- CSharpTraningCourse/BasicCSharp/Program.cs
+++ CSharpTraningCourse/BasicCSharp/Program.cs
@@ -10,6 +10,7 @@
             mySchwarzMachine.LoadWater(1800);
             mySchwarzMachine.LoadGroundedCoffee(1500);
             mySchwarzMachine.MakeCoffee();
+            Console.WriteLine(mySchwarzMachine.ReportAmounts());
             Console.WriteLine("----------------");
 
             var myEsspressoMachine = new EsspressoMachine("Delonghi", "EC685", 1500, 1800);
@@ -18,13 +19,20 @@
             myEsspressoMachine.LoadWater(500);
             myEsspressoMachine.LoadGroundedCoffee(15);
             myEsspressoMachine.MakeCoffee();
+            Console.WriteLine(myEsspressoMachine.ReportAmounts());
             Console.WriteLine("----------------");
 
             var myAutomaticMachine = new AutomaticMachine("Krups", "EA873810", 1800, 700, 600, 900);
             Console.WriteLine(myAutomaticMachine);
             Console.WriteLine();
-
-
+            myAutomaticMachine.LoadWater(1500);
+            myAutomaticMachine.LoadCoffeeBeans(600);
+            myAutomaticMachine.MakeDoubleCoffee();
+            myAutomaticMachine.MakeLatte();
+            myAutomaticMachine.LoadMilk(500);
+            myAutomaticMachine.MakeCapuchino();
+            myAutomaticMachine.MakeLatte();
+            Console.WriteLine(myAutomaticMachine.ReportAmounts());
         }
     }
 }
